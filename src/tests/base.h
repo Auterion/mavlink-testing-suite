f8d37afa885f42f9a92d9fc88f6dda67e57934d8
Julian Oes <julian@oes.ch>
2019-02-04 13:03:42 +0100

    tests: move sfinae to its own header file

--- src/tests/base.h
+++ src/tests/base.h
@@ -2,46 +2,15 @@
 
 #include <dronecode_sdk/dronecode_sdk.h>
 #include <yaml-cpp/yaml.h>
+#include "streamable.h"
 
 #include <iostream>
 #include <map>
 #include <memory>
-#include <sstream>
 #include <string>
 #include <type_traits>
 
 #define UNUSED(x) (void)(x)
-
-// SFINAE to check if something is streamable adapted from:
-// https://stackoverflow.com/questions/29319000
-template <class...>
-struct Voider {
-    using type = void;
-};
-template <class... T>
-using void_t = typename Voider<T...>::type;
-
-template <class T, class = void>
-struct MaybeStreamable {
-    std::string print(const T& thing)
-    {
-        UNUSED(thing);
-        return "";
-    }
-    bool isStreamable() const { return false; }
-};
-
-template <class T>
-struct MaybeStreamable<T, void_t<decltype(std::declval<std::ostream&>() << std::declval<T>())>> {
-    std::string print(const T& thing)
-    {
-        std::stringstream ss;
-        ss << thing;
-        return ss.str();
-    }
-
-    bool isStreamable() const { return true; }
-};
 
 namespace tests
 {
