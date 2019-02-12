f104c1fc9bdb8ad91e91bc6310fb9a40a92cec5b
Julian Oes <julian@oes.ch>
2019-02-12 09:25:59 +0100

    cmake: allow to use local DronecodeSDK artefacts
    For debugging purposes it's handy to build and install the Dronecode
    SDK manually.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,18 +1,27 @@
 include(ExternalProject)
 
-# add SDK
-ExternalProject_Add(third_party_dronecode_sdk
-    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/third_party/DronecodeSDK
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk/install
-        -DENABLE_MAVLINK_PASSTHROUGH=1
-    GIT_REPOSITORY https://github.com/Dronecode/DronecodeSDK.git
-    GIT_TAG 1a62225df0ef4f0f1034fa6979b087e679afd58e
-    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk
-    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
-    )
-include_directories(${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk/install/include)
-link_directories(${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk/install/lib)
+
+if(DRONECODE_SDK_INSTALL_DIR)
+    message(STATUS "Using DronecodeSDK from: ${DRONECODE_SDK_INSTALL_DIR}")
+    include_directories(${DRONECODE_SDK_INSTALL_DIR}/include)
+    link_directories(${DRONECODE_SDK_INSTALL_DIR}/lib)
+else()
+    # clone and build SDK via ExternalProject
+    ExternalProject_Add(third_party_dronecode_sdk
+        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/third_party/DronecodeSDK
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk/install
+            -DENABLE_MAVLINK_PASSTHROUGH=1
+        GIT_REPOSITORY https://github.com/Dronecode/DronecodeSDK.git
+        GIT_TAG 1a62225df0ef4f0f1034fa6979b087e679afd58e
+        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk
+        INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
+        )
+    include_directories(${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk/install/include)
+    link_directories(${CMAKE_CURRENT_BINARY_DIR}/build_dronecode_sdk/install/lib)
+    list(APPEND dependencies third_party_dronecode_sdk)
+endif()
+
 
 # add YAML (build from source)
 ExternalProject_Add(third_party_yaml
@@ -25,3 +34,4 @@ ExternalProject_Add(third_party_yaml
     INSTALL_COMMAND ""
     )
 include_directories(${CMAKE_CURRENT_BINARY_DIR}/third_party/yaml-cpp/include)
+list(APPEND dependencies third_party_yaml)
